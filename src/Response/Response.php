0dcb41119a47f069698026db04f7aac117d290e1
Gábor Jaczkó <gabor.jaczko@switchconcepts.com>
2016-06-28 10:46:52 +0100

    fixed PHP 5.3 compatibility

--- src/Response/Response.php
+++ src/Response/Response.php
@@ -55,7 +55,11 @@ class Response
 
         if (json_last_error() !== JSON_ERROR_NONE) {
             throw new InvalidResponseException(
-                sprintf('%s: %s', json_last_error_msg(), $responseString)
+                sprintf(
+                    '%s: %s',
+                    function_exists('json_last_error_msg') ? json_last_error_msg() : self::getJsonErrorMessage(),
+                    $responseString
+                )
             );
         }
 
@@ -86,5 +90,34 @@ class Response
             isset($responseObject->error) ? Error::fromObject($responseObject->error) : null,
             $responseObject->id
         );
+    }
+
+    /**+
+     * @return string
+     */
+    private static function getJsonErrorMessage()
+    {
+        switch (json_last_error()) {
+            case JSON_ERROR_NONE:
+                return 'No error has occurred';
+            case JSON_ERROR_DEPTH:
+                return 'The maximum stack depth has been exceeded';
+            case JSON_ERROR_STATE_MISMATCH:
+                return 'Invalid or malformed JSON';
+            case JSON_ERROR_CTRL_CHAR:
+                return 'Control character error, possibly incorrectly encoded';
+            case JSON_ERROR_SYNTAX:
+                return 'Syntax error';
+            case JSON_ERROR_UTF8:
+                return 'Malformed UTF-8 characters, possibly incorrectly encoded';
+            case JSON_ERROR_RECURSION:
+                return 'One or more recursive references in the value to be encoded';
+            case JSON_ERROR_INF_OR_NAN:
+                return 'One or more NAN or INF values in the value to be encoded';
+            case JSON_ERROR_UNSUPPORTED_TYPE:
+                return 'A value of a type that cannot be encoded was given';
+            default:
+                return 'Unknown error';
+        }
     }
 }
